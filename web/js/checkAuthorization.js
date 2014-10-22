0e09d9d24c529ee9963adbfbdfffe6e340002128
kulmerov <dim_on@live.ru>
2014-10-22 13:09:39 +0300

    проблемы с кодировкой

--- web/js/checkAuthorization.js
+++ web/js/checkAuthorization.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
     var login = $.cookie("login");
-    if(login == null) {
+    if(login == null || login == "") {
         return false;
     } else {
-        $("#searchInTopbar").find("[href = '/login']").html("<i class='icon-user'></i> Log out (<b>" + login + "</b>)");
+        $("#authorization").html("<i class='icon-user'></i> Log out (<b>" + login + "</b>)").click(function() {
+            $.cookie("login", "");
+            $(this).html("<i class='icon-user'></i> Log in").unbind();
+            return false;
+        });
     }
 });
